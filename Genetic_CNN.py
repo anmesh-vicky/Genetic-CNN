623934578b694ff780b9ef0292ec4ab513b93e53
anmesh-vicky <anmesh231@gmail.com>
2019-04-07 23:44:48 +0530

    filters added drop-out added

--- Genetic_CNN.py
+++ Genetic_CNN.py
@@ -9,7 +9,7 @@
 # 2. <a href="https://github.com/thieman/py-dag"> py-dag</a> for Directed Asyclic Graph (Did few changes for Python 3, check dag.py)
 # 3. Tensorflow
 
-# In[1]:
+# In[30]:
 
 
 import random
@@ -23,7 +23,7 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
 
-# In[2]:
+# In[31]:
 
 
 mnist = input_data.read_data_sets("mnist_data/", one_hot=True)
@@ -36,18 +36,20 @@ train_imgs = np.reshape(train_imgs,[-1,28,28,1])
 test_imgs = np.reshape(test_imgs,[-1,28,28,1])
 
 
-# In[3]:
+# In[32]:
 
 
 train_imgs.shape,test_imgs.shape,train_labels.shape
 # test_imgs
 
 
-# In[12]:
+# In[66]:
 
 
 STAGES = np.array(["s1","s2"]) # S
 NUM_NODES = np.array([3,5]) # K
+depth=np.array([20,50])
+chn=np.array([20,50])
 
 L =  0 # genome length
 BITS_INDICES, l_bpi = np.empty((0,2),dtype = np.int32), 0 # to keep track of bits for each stage S
@@ -59,11 +61,11 @@ for nn in NUM_NODES:
 L = int(0.5 * L)
 
 TRAINING_EPOCHS = 20
-BATCH_SIZE = 20
+BATCH_SIZE = 32
 TOTAL_BATCHES = train_imgs.shape[0] // BATCH_SIZE
 
 
-# In[16]:
+# In[67]:
 
 
 def initial_learning_rate(epoch=0):
@@ -73,10 +75,9 @@ def initial_learning_rate(epoch=0):
         return 0.00001
     if (epoch >= 16):
         return 0.000001
-epoch=0
 
 
-# In[17]:
+# In[68]:
 
 
 def weight_variable(weight_name, weight_shape):
@@ -156,12 +157,15 @@ def generate_tensorflow_graph(individual,stages,num_nodes,bits_indices):
     Y = tf.placeholder(tf.float32,[None,10],name = "Y")
         
     d_node = X
-    for stage_name,num_node,bpi in zip(stages,num_nodes,bits_indices):
+    c=0
+    for stage_name,num_node,bpi ,d1,n1 in zip(stages,num_nodes,bits_indices,depth,chn):
         indv = individual[bpi[0]:bpi[1]]
-
-        add_node(''.join([stage_name,"_input"]),d_node.name)
+        if stage_name == "s1":
+            add_node(''.join([stage_name,"_input"]),d_node.name,d=d1)
+        else:
+            add_node(''.join([stage_name,"_input"]),d_node.name,nc=20,d=d1)
         pooling_layer_name = ''.join([stage_name,"_input",activation_function_pattern])
-
+#
         if not has_same_elements(indv):
             # ------------------- Temporary DAG to hold all connections implied by GA solution ------------- #  
 
@@ -177,7 +181,7 @@ def generate_tensorflow_graph(individual,stages,num_nodes,bits_indices):
             # --------------------------- Initialize tensforflow graph based on DAG ------------------------- #
 
             for wop in without_predecessors:
-                add_node(wop,''.join([stage_name,"_input",activation_function_pattern]))
+                add_node(wop,''.join([stage_name,"_input",activation_function_pattern]),nc=n1,d=d1)
 
             for n in nodes:
                 predecessors = dag.predecessors(n)
@@ -188,32 +192,34 @@ def generate_tensorflow_graph(individual,stages,num_nodes,bits_indices):
                     for prd in range(1,len(predecessors)):
                         t = sum_tensors(first_predecessor,predecessors[prd],activation_function_pattern)
                         first_predecessor = t.name
-                    add_node(n,first_predecessor)
+                    add_node(n,first_predecessor,nc=n1,d=d1)
                 elif predecessors:
-                    add_node(n,''.join([predecessors[0],activation_function_pattern]))
+                    add_node(n,''.join([predecessors[0],activation_function_pattern]),nc=n1,d=d1)
 
             if len(without_successors) > 1:
                 first_successor = without_successors[0]
                 for suc in range(1,len(without_successors)):
                     t = sum_tensors(first_successor,without_successors[suc],activation_function_pattern)
                     first_successor = t.name
-                add_node(''.join([stage_name,"_output"]),first_successor) 
+                add_node(''.join([stage_name,"_output"]),first_successor,nc=n1,d=d1) 
             else:
-                add_node(''.join([stage_name,"_output"]),''.join([without_successors[0],activation_function_pattern])) 
+                add_node(''.join([stage_name,"_output"]),''.join([without_successors[0],activation_function_pattern]),nc=n1,d=d1) 
 
             pooling_layer_name = ''.join([stage_name,"_output",activation_function_pattern])
             # ------------------------------------------------------------------------------------------ #
 
         d_node =  apply_pool(tf.get_default_graph().get_tensor_by_name(pooling_layer_name), 
-                                 kernel_height = 16, kernel_width = 16,stride_size = 2)
+                                 kernel_height = 5, kernel_width = 5,stride_size = 2)
 
     shape = d_node.get_shape().as_list()
     flat = tf.reshape(d_node, [-1, shape[1] * shape[2] * shape[3]])
-    logits = linear_layer(flat,10,"logits")
+    drop=tf.layers.dropout(flat,rate=0.5)
+    logits = linear_layer(drop,10,"logits")
     
     xentropy =  tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = Y)
     loss_function = tf.reduce_mean(xentropy)
-    optimizer = tf.train.AdamOptimizer(learning_rate=initial_learning_rate(epoch)).minimize(loss_function) 
+    learning_rate_placeholder = tf.placeholder(tf.float32, [], name='learning_rate')
+    optimizer = tf.train.AdamOptimizer(learning_rate=.0001).minimize(loss_function) 
     accuracy = tf.reduce_mean(tf.cast( tf.equal(tf.argmax(tf.nn.softmax(logits),1), tf.argmax(Y,1)), tf.float32))
     
     return  X, Y, optimizer, loss_function, accuracy
@@ -230,17 +236,15 @@ def evaluateModel(individual):
                 offset = (epoch * BATCH_SIZE) % (train_labels.shape[0] - BATCH_SIZE)
                 batch_x = train_imgs[offset:(offset + BATCH_SIZE), :, :, :]
                 batch_y = train_labels[offset:(offset + BATCH_SIZE), :]
-                _, c = session.run([optimizer, loss_function],feed_dict={X: batch_x, Y : batch_y})
+                _, c = session.run([optimizer,loss_function],feed_dict={X: batch_x, Y : batch_y})
                 loss, acc = session.run([loss_function, accuracy], feed_dict={X: batch_x,Y: batch_y})
-#                 print("Iter " + str(b) + ", Loss= " + \
-#                       "{:.6f}".format(loss) + ", Training Accuracy= " + \
-#                       "{:.5f}".format(acc))
-            print(epoch)
+            print("Iter " + str(epoch) + ", Loss= " +                       "{:.6f}".format(loss) + ", Training Accuracy= " +                       "{:.5f}".format(acc))
+            
 #         print("Optimization Finished!")
                 
         #writer = tf.summary.FileWriter('./graphs', session.graph)   
-        score = session.run(accuracy, feed_dict={X: test_imgs, Y: test_labels})
-        print('Accuracy: ',score)
+            score = session.run(accuracy, feed_dict={X: test_imgs, Y: test_labels})
+            print('Accuracy: ',score)
     return score,
 
 
@@ -264,6 +268,7 @@ toolbox.register("select", tools.selRoulette)
 toolbox.register("evaluate", evaluateModel)
 
 popl = toolbox.population(n = population_size)
+print(popl)
 result = algorithms.eaSimple(popl, toolbox, cxpb = 0.4, mutpb = 0.05, ngen = num_generations, verbose = True)
 
 
